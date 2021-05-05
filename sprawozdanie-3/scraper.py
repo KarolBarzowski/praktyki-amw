ede973e2c56201ab683af77f87c579c6ae25e2e3
Karol Barzowski <gamekarol@gmail.com>
2021-05-05 15:19:23 +0200

    Scraper finished

--- sprawozdanie-3/scraper.py
+++ sprawozdanie-3/scraper.py
@@ -1,17 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
 
-result = requests.get("https://apynews.pl/ranking-youtuberow/compact")
-src = result.content
-soup = BeautifulSoup(src, "html.parser")
+data = []
 
-rows = soup.find_all("tr")[1:101]
+def scrap(n):
+    result = requests.get("https://apynews.pl/ranking-youtuberow/compact")
+    src = result.content
+    soup = BeautifulSoup(src, "html.parser")
 
-for row in rows:
-    place = row.find("td", class_="tall").text
-    nickname = row.find("td", class_="name-cell").text
-    subs = row.find("strong").text
-    views = row.find_all("strong")[1].text
+    rows = soup.find_all("tr")[1:n + 1]
 
-    print(place, nickname, subs, views)
+    for row in rows:
+        place = row.find("td", class_="tall").text
+        nickname = row.find("td", class_="name-cell").text
+        subs = row.find("strong").text
+        views = row.find_all("strong")[1].text
+
+        data.append((
+            place,
+            nickname,
+            subs,
+            views
+        ))
+
+def display():
+    for place, nickname, subs, views in data:
+        print(place, nickname, subs, views)
+
+print("Ranking youtuberow")
+amount = 0
+while amount < 1 or amount > 300:
+    amount = int(input("Chce zobaczyc TOP (1-300): "))
+
+scrap(amount)
+display()
