3d3ee600b597446241422f5bf2c32991cff54e1d
Karol Barzowski <gamekarol@gmail.com>
2021-05-05 14:40:37 +0200

    Added scraping: place, nickname, subs, views

--- sprawozdanie-3/scraper.py
+++ sprawozdanie-3/scraper.py
@@ -5,3 +5,13 @@ import csv
 result = requests.get("https://apynews.pl/ranking-youtuberow/compact")
 src = result.content
 soup = BeautifulSoup(src, "html.parser")
+
+rows = soup.find_all("tr")[1:101]
+
+for row in rows:
+    place = row.find("td", class_="tall").text
+    nickname = row.find("td", class_="name-cell").text
+    subs = row.find("strong").text
+    views = row.find_all("strong")[1].text
+
+    print(place, nickname, subs, views)
